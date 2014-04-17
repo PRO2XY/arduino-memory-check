0b55a2704061146a743066c5f8a047adc4c8fdd4
Pranav Sharma <pranavsharma2504@gmail.com>
2014-04-17 08:05:45 +0530

    Create MemoryFree.h

---
+++ library/MemoryFree/MemoryFree.h
@@ -0,0 +1,21 @@
+/*****************
+*  Author: mem
+*  Source: http://forum.arduino.cc/index.php?PHPSESSID=jbd9v8sv2re9s9n7j15o8e8r50&topic=27536.15
+*****************/
+
+// memoryFree header
+
+#ifndef      MEMORY_FREE_H
+#define MEMORY_FREE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int freeMemory();
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
